d73eae8263a0102baf25b68213b065d7a7fb2e3c
Ankur Seth <iankurseth@gmail.com>
2020-10-25 20:18:37 +0530

    Added string constants

--- lib/orepl.c
+++ lib/orepl.c
@@ -3,16 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 
+char *OREPL_WELCOME_TEXT = "Welcome to Orepl!";
+char *OREPL_FAREWELL_TEXT = "Bye.";
+
+char *OREPL_COMMAND_EXIT = "exit";
+
+
+
 void
 orepl ()
 {
 	char input[OREPL_MAX_INPUT_LENGTH] = "";
 
-	while (strcmp(input, "exit")) {
-		printf("Welcome to Orepl!\n");
+	while (strcmp(input, OREPL_COMMAND_EXIT)) {
+		printf("%s\n", OREPL_WELCOME_TEXT);
 		printf("=> ");
 		scanf("%s", input);
 	}
+	printf("%s\n", OREPL_FAREWELL_TEXT);
 }
 
 
