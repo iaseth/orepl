e8772c3f8a5ece0a4243ae0522ffb0d9f5dc8c5a
Ankur Seth <iankurseth@gmail.com>
2020-10-25 22:55:23 +0530

    Updated loop structure

--- lib/orepl.c
+++ lib/orepl.c
@@ -15,10 +15,13 @@ orepl ()
 {
 	char input[OREPL_MAX_INPUT_LENGTH] = "";
 
-	while (strcmp(input, OREPL_COMMAND_EXIT)) {
-		printf("%s\n", OREPL_WELCOME_TEXT);
+	printf("%s\n", OREPL_WELCOME_TEXT);
+	while (1) {
 		printf("=> ");
 		scanf("%s", input);
+		if (strcmp(input, OREPL_COMMAND_EXIT) == 0) {
+			break;
+		}
 	}
 	printf("%s\n", OREPL_FAREWELL_TEXT);
 }
