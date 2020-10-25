da3539f8948de7bbe84eaef47019dc546c7595a9
Ankur Seth <iankurseth@gmail.com>
2020-10-26 02:59:10 +0530

    Moved commands to separate file

---
+++ include/orepl_commands.h
@@ -0,0 +1,18 @@
+#ifndef OREPL_COMMANDS_H_IS_INCLUDED
+#define OREPL_COMMANDS_H_IS_INCLUDED
+
+
+
+char *OREPL_WELCOME_TEXT = "Welcome to Orepl!";
+char *OREPL_FAREWELL_TEXT = "Bye.";
+
+char *OREPL_COMMAND_BREAK   = "break";
+char *OREPL_COMMAND_EXIT    = "exit";
+char *OREPL_COMMAND_HELP    = "help";
+char *OREPL_COMMAND_LOAD    = "load";
+char *OREPL_COMMAND_SAVE    = "save";
+char *OREPL_COMMAND_VERSION = "version";
+
+
+
+#endif
